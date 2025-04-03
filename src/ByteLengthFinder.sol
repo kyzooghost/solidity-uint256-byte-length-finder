7e84481d08df99994a6d0584fa8be3bb822a7f39
kyzooghost <jeffersonzjtang@gmail.com>
2025-04-04 00:39:34 +1100

    did tests

--- src/ByteLengthFinder.sol
+++ src/ByteLengthFinder.sol
@@ -13,51 +13,40 @@ contract ByteLengthFinder {
             // Starting index of byte section we will check, 0=MSB, 31=LSB
             // We start from MSB, and so we greedily check the most significant byte section that we have not ruled out
             let sectionStart := 0
-            // Length in bytes of section we will check
-            let sectionLength := 16
+            // Length in bits of section we will check
+            let sectionLength := 128
             for {
-            // TODO - Check this ending condition
-            } gt(sectionLength, 0) {
+            } gt(sectionLength, 7) {
                 // Halve sectionLength with each round
                 sectionLength := shr(1, sectionLength)
             } {
-                let section := shl(mul(8, sectionStart), x)
-                log1(0x00, 0, section)
-                section := shr(mul(8, sectionStart), section)
-                log1(0x00, 0, section)
-                section := shr(mul(8, sectionLength), section)
-                log1(0x00, 0, section)
+                let section := shr(sub(256, add(sectionLength, sectionStart)), x)
                 // This byte section is 0 -> in next round, byte section starts where the current round's section ended
-                
-                log3(0x00, 0, sectionStart, sectionLength, section)
                 if iszero(section) {
                     sectionStart := add(sectionStart, sectionLength)
                     continue
                 }
                 // This byte section is not-zero -> in next round, byte section starts where current round's section started
             }
-            mstore(0x00, sub(32, sectionStart))
+            mstore(0x00, div(sub(256, sectionStart), 8))
+            return(0x00, 0x20)
+        }
+    }
+
+    function getUint256ByteLengthWithLinearSearch(uint256 x) external returns (uint256) {
+        assembly {
+            let i := 0
+            for {
+            } lt(i, 32) {
+                i := add(i, 1)
+            } {
+                if iszero(and(shr(sub(248, mul(i, 8)), x), 0xff)) {
+                    continue
+                }
+                break
+            }
+            mstore(0x00, sub(32, i))
             return(0x00, 0x20)
         }
     }
 }
-
-// R1
-// 0000-0000-0000-0000-0000-0000-0000-0XXX
-// sS = 0, sL = 16
-
-// R2
-// 0000-0000-0000-0XXX
-// sS = 16, sL = 8
-
-// R3
-// 0000-0XXX
-// sS = 24, sL = 4
-
-// R4
-// 0XXX
-// sS = 28, sL = 2
-
-    // │   ├─  emit topic 0: 0x000000000000000000000000000000000000000000000000000000000000001c
-    // │   │        topic 1: 0x0000000000000000000000000000000000000000000000000000000000000002
-    // │   │        topic 2: 0x0000000000000000000000000000000000000000000000000000000000000000
