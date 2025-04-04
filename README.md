87a32023bab38a16fc7a9eb2b54296c5fcba928f
kyzooghost <jeffersonzjtang@gmail.com>
2025-04-04 15:11:24 +1100

    better documentation

--- README.md
+++ README.md
@@ -12,4 +12,18 @@ Compare with linear search implementation
 
 `forge test`
 
+## Gas test results
 
+Profiled with `forge test`
+
+|            number            | binary search gas cost | linear search gas cost |
+|------------------------------|------------------------|------------------------|
+| 1                            | 8933                   | 11060                  |
+| 10                           | 8899                   | 11006                  |
+| 100                          | 8922                   | 11017                  |
+| 1000                         | 8899                   | 10952                  |
+| 10000                        | 8866                   | 10997                  |
+| 100000                       | 8921                   | 10836                  |
+| 10 ** 18                     | 8878                   | 10434                  |
+| 10 ** 40                     | 8854                   | 9645                   |
+| (2 ** 256) - 1               | 8812                   | 8373                   |
--- test/ByteLengthFinder.t.sol
+++ test/ByteLengthFinder.t.sol
@@ -22,6 +22,22 @@ contract ByteLengthFinderTest is Test {
         assertEq(c.getUint256ByteLengthWithLinearSearch(1), 1);
     }
 
+    function test_binary_10() public {
+        assertEq(c.getUint256ByteLengthWithBinarySearch(10), 1);
+    }
+
+    function test_linear_10() public {
+        assertEq(c.getUint256ByteLengthWithLinearSearch(10), 1);
+    }
+
+    function test_binary_100() public {
+        assertEq(c.getUint256ByteLengthWithBinarySearch(100), 1);
+    }
+
+    function test_linear_100() public {
+        assertEq(c.getUint256ByteLengthWithLinearSearch(100), 1);
+    }
+
     function test_binary_1000() public {
         assertEq(c.getUint256ByteLengthWithBinarySearch(1000), 2);
     }
@@ -30,12 +46,36 @@ contract ByteLengthFinderTest is Test {
         assertEq(c.getUint256ByteLengthWithLinearSearch(1000), 2);
     }
 
-    function test_binary_9000() public {
-        assertEq(c.getUint256ByteLengthWithBinarySearch(9000), 2);
+    function test_binary_10000() public {
+        assertEq(c.getUint256ByteLengthWithBinarySearch(10000), 2);
     }
 
-    function test_linear_9000() public {
-        assertEq(c.getUint256ByteLengthWithLinearSearch(9000), 2);
+    function test_linear_10000() public {
+        assertEq(c.getUint256ByteLengthWithLinearSearch(10000), 2);
+    }
+
+    function test_binary_100000() public {
+        assertEq(c.getUint256ByteLengthWithBinarySearch(100000), 3);
+    }
+
+    function test_linear_100000() public {
+        assertEq(c.getUint256ByteLengthWithLinearSearch(100000), 3);
+    }
+
+    function test_binary_10_pow_18() public {
+        assertEq(c.getUint256ByteLengthWithBinarySearch(10**18), 8);
+    }
+
+    function test_linear_10_pow_18() public {
+        assertEq(c.getUint256ByteLengthWithLinearSearch(10**18), 8);
+    }
+
+    function test_binary_10_pow_40() public {
+        assertEq(c.getUint256ByteLengthWithBinarySearch(10**40), 17);
+    }
+
+    function test_linear_10_pow_40() public {
+        assertEq(c.getUint256ByteLengthWithLinearSearch(10**40), 17);
     }
 
     function test_binary_maxUint256() public {
@@ -46,11 +86,5 @@ contract ByteLengthFinderTest is Test {
         assertEq(c.getUint256ByteLengthWithLinearSearch(type(uint256).max), 32);
     }
 
-    function test_binary_9489014812984912() public {
-        assertEq(c.getUint256ByteLengthWithBinarySearch(9489014812984912), 7);
-    }
 
-    function test_linear_9489014812984912() public {
-        assertEq(c.getUint256ByteLengthWithLinearSearch(9489014812984912), 7);
-    }
 }
